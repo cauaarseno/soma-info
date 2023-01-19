0e596c496b4624925aef37ab24758c1ebad8fbf0
Cauã Arseno <cauaarseno61@gmail.com>
2023-01-18 23:08:23 -0300

    adicionando subtração

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,4 +39,13 @@ class HomeController extends Controller
 
         return $soma;
     }
+
+    public function sub($num1, $num2)
+    {
+        $sub = $num1 - $num2;
+        logger()->debug('Sub feita', compact('num1', 'num2', 'sub'));
+
+        return $sub;
+    }
+    
 }
